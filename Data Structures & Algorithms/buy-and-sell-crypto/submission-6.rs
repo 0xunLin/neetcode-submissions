ef7122335fe1cd80367b0c58ea870467e943e0b9
Shaun <72510772+0xunLin@users.noreply.github.com>
2026-07-23 14:18:16 +0530

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.rs
@@ -0,0 +1,21 @@
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let mut left = 0;
+        let mut right = left+1;
+
+        let mut max_profit = 0;
+
+        while right<prices.len(){
+            if prices[left]<prices[right] {
+                max_profit = max_profit.max(prices[right]-prices[left]);
+
+            } else {
+                left = right;
+            }
+
+            right += 1;
+        }
+
+        max_profit
+    }
+}
