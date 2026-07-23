d2467a58ee8bac1953fa0ca6b4fc9c6e89719a62
Shaun <72510772+0xunLin@users.noreply.github.com>
2026-07-24 02:20:05 +0530

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.rs
@@ -0,0 +1,27 @@
+impl Solution {
+    pub fn character_replacement(s: String, k: i32) -> i32 {
+        let bytes = s.as_bytes();
+
+        let mut left = 0;
+        let mut freq = [0;26]; // Tracks counts of uppercase letters A-Z
+        let mut max_freq = 0;
+        let mut best = 0;
+
+        for right in 0..bytes.len() {
+            let right_index = (bytes[right]-b'A') as usize;
+            freq[right_index] += 1;
+
+            max_freq = max_freq.max(freq[right_index]);
+
+            if ((right-left+1) as i32) - max_freq > k {
+                let left_index = (bytes[left]-b'A') as usize;
+                freq[left_index] -= 1;
+                left += 1;
+
+            }
+            best = best.max(right-left+1)
+
+        }
+        best as i32
+    }
+}
