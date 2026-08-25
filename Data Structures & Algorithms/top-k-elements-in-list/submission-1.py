b767d22ec0c150537c54809d923a229a0bee542c
Priyantar Jonak <72510772+0xunLin@users.noreply.github.com>
2026-08-25 11:17:35 +0530

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -0,0 +1,35 @@
+# import collections
+# import heapq
+
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#         count = collections.Counter(nums)
+#         sorted_elements = sorted(count.keys(), key=count.get, reverse=True)
+#         return sorted_elements[:k]
+
+
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#         count = collections.Counter(nums)
+#         min_heap = []
+#         for num, freq in count.items():
+#             heapq.heappush(min_heap, (freq,num))
+#             if len(min_heap) > k:
+#                 heapq.heappop(min_heap)
+#         return [num for freq, num in min_heap]
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+        buckets = [[] for _ in range(len(nums) + 1)]
+        for num, freq in count.items():
+            buckets[freq].append(num)
+        result = []
+        for i in range(len(buckets) - 1, 0, -1):
+            for num in buckets[i]:
+                result.append(num)
+                if len(result) == k:
+                    return result
