a21165362ab5caf680040839dc8ac588b94b43e0
Priyantar Jonak <72510772+0xunLin@users.noreply.github.com>
2026-09-03 11:51:32 +0530

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count_map = {}
+        left = 0
+        max_len = 0
+        max_freq = 0
+        for right in range(len(s)):
+            count_map[s[right]] = count_map.get(s[right], 0) + 1 # Update running state
+            max_freq = max(max_freq, count_map[s[right]]) # Update running state
+            while (right - left + 1) - max_freq > k: # Invalid state, remove an instance of s[left] and move/increment left pointer by 1
+                count_map[s[left]] -= 1
+                left += 1
+            max_len = max(max_len, right - left + 1) # maximum window size between current and right-left+1
+        return max_len
